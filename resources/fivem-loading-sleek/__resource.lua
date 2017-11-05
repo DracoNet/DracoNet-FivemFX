1c1f8990d24d52f09823b3c2a058f030f6fdc306
Draco Dragon <d@dracomail.net>
2017-11-06 00:24:49 +0100

    UI fix
    LoadingScreen
    Job display

---
+++ resources/fivem-loading-sleek/__resource.lua
@@ -0,0 +1,22 @@
+--
+-- Author: @NicolasStr_
+-- If you have any issue, please consider opening a issue on Github
+--
+files {
+    'files/index.html',
+    'files/config.js',
+    'files/infos.txt',
+	'files/sound.ogg',
+    'files/img/bg1.jpg',
+    'files/img/bg2.jpg',
+    'files/img/bg3.jpg',
+	'files/img/bg4.jpg',
+	'files/img/bg5.jpg',
+	'files/img/bg6.jpg',
+	'files/img/bg7.jpg',
+    'files/lib/pricedown.ttf',
+    'files/lib/script.js',
+    'files/lib/style.css'
+}
+
+loadscreen 'files/index.html'
