6927edeef7dd713fb26c4cc882ecb074cef6c1cd
Draco Dragon <d@dracomail.net>
2017-11-21 02:16:59 +0100

    Resources update

--- resources/[system]/chat/html/config.default.js
+++ resources/[system]/chat/html/config.default.js
@@ -8,10 +8,10 @@ window.CONFIG = {
     'defaultAlt': '{0}',
     'example:important': '<h1>^2{0}</h1>'
   },
-  fadeTimeout: 10000,
+  fadeTimeout: 7000,
   suggestionLimit: 5,
   style: {
-    background: 'rgba(52, 73, 94, 0.1)',
+    background: 'rgba(52, 73, 94, 0.7)',
     width: '38%',
     height: '240px',
   }
